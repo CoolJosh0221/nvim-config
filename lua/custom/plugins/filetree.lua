5125a655e0f283ca3b9dc0fd5012f71239feada2
CoolJosh0221 <joshluffy99@gmail.com>
2023-08-13 21:56:18 +0800

    NvFileTree

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,55 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    {
+      -- only needed if you want to use the commands with "_with_window_picker" suffix
+      's1n7ax/nvim-window-picker',
+      tag = "*",
+      config = function()
+        require 'window-picker'.setup({
+          autoselect_one = true,
+          include_current = false,
+          filter_rules = {
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', "quickfix" },
+            },
+          },
+          other_win_hl_color = '#e35e4f',
+        })
+      end,
+    }
+  },
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- Change type
+            added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted   = "✖", -- this can only be used in the git_status source
+            renamed   = "󰁕", -- this can only be used in the git_status source
+            -- Status type
+            untracked = "",
+            ignored   = "",
+            unstaged  = "󰄱",
+            staged    = "",
+            conflict  = "",
+          }
+        },
+      }
+    }
+  end,
+}
